01875b7da6dfefc68065d71c50fae77259fd6037
zk9059 <zk9059@rit.edu>
2024-02-08 20:09:29 +0400

    test triangle

--- test.py
+++ test.py
@@ -3,9 +3,18 @@ import turtle
 
 
 #here we will be creating a file for the Assignment for GCIS123
+homex=0
+homey=0
 
+#this will create a triangle as test
 def turtleDrawing():
     turtle.goto(50,50)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.left(100)
+    turtle.goto(homex,homey)
+
+
 
 
 turtleDrawing()
