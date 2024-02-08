1fbc077de2a66e1c989ab6eec48c0bdffabff539
zk9059 <zk9059@rit.edu>
2024-02-08 20:20:00 +0400

    test2

--- test.py
+++ test.py
@@ -7,14 +7,27 @@ homex=0
 homey=0
 
 #this will create a triangle as test
+
+
+def main():
+    turtleDrawing()
+
+
 def turtleDrawing():
     turtle.goto(50,50)
     turtle.right(90)
     turtle.forward(100)
     turtle.left(100)
     turtle.goto(homex,homey)
+    turtle.goto(100,100)
+    turtle.forward(40)
 
 
 
 
-turtleDrawing()
+main()
+
+
+
+
+
